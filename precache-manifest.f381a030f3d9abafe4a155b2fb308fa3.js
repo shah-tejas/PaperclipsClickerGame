92b4446a0aea42e1984f91a443f2bbb22943401d
Tejas Shah <shah.te@husky.neu.edu>
2020-06-10 15:55:02 -0400

    Updates

---
+++ precache-manifest.f381a030f3d9abafe4a155b2fb308fa3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c983a6f9c9a67d40d303b080de300c8c",
+    "url": "/PaperclipsClickerGame/index.html"
+  },
+  {
+    "revision": "6b669306056ca04ccd2f",
+    "url": "/PaperclipsClickerGame/static/css/main.3772d5ed.chunk.css"
+  },
+  {
+    "revision": "54873445fb70329782eb",
+    "url": "/PaperclipsClickerGame/static/js/2.e5c51c6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PaperclipsClickerGame/static/js/2.e5c51c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b669306056ca04ccd2f",
+    "url": "/PaperclipsClickerGame/static/js/main.5b32780b.chunk.js"
+  },
+  {
+    "revision": "a5419a9151292910a600",
+    "url": "/PaperclipsClickerGame/static/js/runtime-main.4a01e588.js"
+  },
+  {
+    "revision": "c07f374e06cafcf223953505101e5105",
+    "url": "/PaperclipsClickerGame/static/media/ai.c07f374e.webp"
+  },
+  {
+    "revision": "670a3d0350162035be2e5c0c371e591f",
+    "url": "/PaperclipsClickerGame/static/media/paperclip.670a3d03.jpg"
+  }
+]);
